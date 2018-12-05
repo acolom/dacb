155140581f137bcd9666aa5e423a1cce936594ec
unknown <alberto.colom.monfort@gmail.com>
2018-12-05 10:31:58 +0100

    Mover precendeces to syntax facts

--- dacbCompiler/CodeAnalysis/Parser.cs
+++ dacbCompiler/CodeAnalysis/Parser.cs
@@ -68,7 +68,7 @@ namespace Dacb.CodeAnalysis
             while(true)
             {
 
-                var precedence = GetBinaryOperatorPrecedence(Current.Kind);
+                var precedence = Current.Kind.GetBinaryOperatorPrecedence();
                 if (precedence == 0 || precedence <= parentPrecedence)
                     break;
                 var operatorToken =  NextToken();
@@ -76,21 +76,6 @@ namespace Dacb.CodeAnalysis
                 return new BinaryExpressionSyntax(left, operatorToken, right);
             }
             return left;
-        }
-
-        private static int GetBinaryOperatorPrecedence(SyntaxKind kind)
-        {
-            switch(kind)
-            {
-                case SyntaxKind.StarToken:
-                case SyntaxKind.SlashToken:
-                    return 2;
-                case SyntaxKind.PlusToken:
-                case SyntaxKind.MinusToken:
-                    return 1;
-                default:
-                    return 0;
-            }
         }
 
         private ExpressionSyntax ParsePrimaryExpression()
