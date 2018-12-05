cfe165feedf5b9318c1f194b20bbad29c17d9482
unknown <alberto.colom.monfort@gmail.com>
2018-12-05 10:11:22 +0100

    Lexer

--- dacbCompiler/CodeAnalysis/Lexer.cs
+++ dacbCompiler/CodeAnalysis/Lexer.cs
@@ -28,7 +28,7 @@ namespace Dacb.CodeAnalysis
             _position++;
         }
 
-        public SyntaxToken NextToken() 
+        public SyntaxToken Lex() 
         {
             //numbers
             // + - * / ( )
@@ -64,19 +64,22 @@ namespace Dacb.CodeAnalysis
                 return new SyntaxToken(SyntaxKind.WhitespaceToken, start, text, null);
             }
 
-            if (Current == '+')
-                return new SyntaxToken(SyntaxKind.PlusToken, _position++, "+", null);
-            else if (Current == '-')
-                return new SyntaxToken(SyntaxKind.MinusToken, _position++, "-", null);
-            else if (Current == '*')
-                return new SyntaxToken(SyntaxKind.StarToken, _position++, "*", null);
-            else if (Current == '/')
-                return new SyntaxToken(SyntaxKind.SlashToken, _position++, "/", null);
-            else if (Current == '(')
-                return new SyntaxToken(SyntaxKind.OpenParanthesisToken, _position++, "(", null);
-            else if (Current == ')')
-                return new SyntaxToken(SyntaxKind.CloseParanthesisToken, _position++, ")", null);
-            
+            switch (Current)
+            {
+                case '+':
+                    return new SyntaxToken(SyntaxKind.PlusToken, _position++, "+", null);
+                case '-':
+                    return new SyntaxToken(SyntaxKind.MinusToken, _position++, "-", null);
+                case '*':
+                    return new SyntaxToken(SyntaxKind.StarToken, _position++, "*", null);
+                case '/':
+                    return new SyntaxToken(SyntaxKind.SlashToken, _position++, "/", null);
+                case '(':
+                    return new SyntaxToken(SyntaxKind.OpenParanthesisToken, _position++, "(", null);
+                case ')':
+                    return new SyntaxToken(SyntaxKind.CloseParanthesisToken, _position++, ")", null);
+            }
+
             _diagnostics.Add($"ERROR: Bad character input: '{Current}'");
             return new SyntaxToken(SyntaxKind.BadToken, _position++, _text.Substring(_position -1, 1), null);
         }
