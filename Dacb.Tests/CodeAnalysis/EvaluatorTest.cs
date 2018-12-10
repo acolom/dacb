25cd9c793ae195a8e6bea19d9f42c96559ae0ca0
unknown <alberto.colom.monfort@gmail.com>
2018-12-10 22:44:59 +0100

    Añadidos test para la evaluacion de resultados

--- Dacb.Tests/CodeAnalysis/EvaluatorTest.cs
+++ Dacb.Tests/CodeAnalysis/EvaluatorTest.cs
@@ -16,12 +16,20 @@ namespace Dacb.Tests.CodeAnalysis
         [InlineData("12 - 3", 9)]
         [InlineData("4 * 2", 8)]
         [InlineData("9 / 3", 3)]
-        [InlineData("-1", -1)]
         [InlineData("(10)", 10)]
+        [InlineData("12 == 12", true)]
+        [InlineData("12 == 3", false)]
+        [InlineData("12 != 12", false)]
+        [InlineData("12 != 3", true)]
+        [InlineData("false == false", true)]
+        [InlineData("true == false", false)]
+        [InlineData("false != false", false)]
+        [InlineData("true != false", true)]
         [InlineData("true", true)]
         [InlineData("false", false)]
         [InlineData("!true", false)]
         [InlineData("!false", true)]
+        [InlineData("(a = 10) * a", 100)]
         public void SyntaxFacts_RoundTrips(string text, object expectedValue) 
         {
             var syntaxTree = SyntaxTree.Parse(text);
