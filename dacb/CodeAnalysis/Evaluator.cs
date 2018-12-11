85757c6f687474d3c4aef4277704110a701b99c9
unknown <alberto.colom.monfort@gmail.com>
2018-12-11 09:17:56 +0100

    Limpieza del Evaluator para trabajar con un switch mas limpio

--- dacb/CodeAnalysis/Evaluator.cs
+++ dacb/CodeAnalysis/Evaluator.cs
@@ -23,69 +23,92 @@ namespace Dacb.CodeAnalysis
 
         private object EvaluateExression(BoundExpression node)
         {
-            if (node is BoundLiteralExpression n)
+            if (node is BoundLiteralExpression)
             {
-                return n.Value;
+                return EvaluateLiteralExpression((BoundLiteralExpression)node);
             }
 
-            if (node is BoundVariableExpression v)
+            else if (node is BoundVariableExpression)
             {
-                return _variables[v.Variable];
+                return EvaluateVariableExpression((BoundVariableExpression)node);
             }
 
+            else if (node is BoundAssignmentExpression)
+            {
+                return EvaluateAssignmentExpression((BoundAssignmentExpression)node);
+            }
+
+            else if (node is BoundUnaryExpression)
+            {
+                return EvaluateUnaryExpression((BoundUnaryExpression)node);
+
+            }
+            else if (node is BoundBinaryExpression)
+            {
+                return EvaluateBinaryExpression((BoundBinaryExpression)node);
+            }
+            else 
+            {
+                throw new Exception($"Unexpected node: {node.Kind}");
+            }
             
-            if (node is BoundAssignmentExpression a)
-            {
-                var value = EvaluateExression(a.Expresion);
-                _variables[a.Variable] = value;
-                return value;
-            }
+        }
 
-
-            if (node is BoundUnaryExpression u)
+        private static object EvaluateLiteralExpression(BoundLiteralExpression n)
+        {
+            return n.Value;
+        }
+        private object EvaluateVariableExpression(BoundVariableExpression v)
+        {
+            return _variables[v.Variable];
+        }
+        private object EvaluateAssignmentExpression(BoundAssignmentExpression a)
+        {
+            var value = EvaluateExression(a.Expresion);
+            _variables[a.Variable] = value;
+            return value;
+        }
+        private object EvaluateUnaryExpression(BoundUnaryExpression u)
+        {
+            var operand = EvaluateExression(u.Operand);
+            switch (u.Op.Kind)
             {
-                var operand = EvaluateExression(u.Operand);
-                switch (u.Op.Kind)
-                {
-                    case BoundUnaryOperatorKind.Identity:
-                        return  (int)operand;
-                    case BoundUnaryOperatorKind.Negation:
-                        return -(int)operand;
-                    case BoundUnaryOperatorKind.LogicalNegation:
-                        return !(bool)operand;
-                    
-                    default:
-                        throw new Exception($"Unexpected unary operator '{u.Op}'");
-                }
+                case BoundUnaryOperatorKind.Identity:
+                    return (int)operand;
+                case BoundUnaryOperatorKind.Negation:
+                    return -(int)operand;
+                case BoundUnaryOperatorKind.LogicalNegation:
+                    return !(bool)operand;
 
+                default:
+                    throw new Exception($"Unexpected unary operator '{u.Op}'");
             }
-            if (node is BoundBinaryExpression b)
+        }
+        private object EvaluateBinaryExpression(BoundBinaryExpression b)
+        {
+            var left = EvaluateExression(b.Left);
+            var right = EvaluateExression(b.Right);
+            switch (b.Op.Kind)
             {
-                var left =  EvaluateExression(b.Left);
-                var right = EvaluateExression(b.Right);
-                switch (b.Op.Kind)
-                {
-                    case BoundBinaryOperatorKind.Addition:
-                        return (int)left + (int)right;
-                    case BoundBinaryOperatorKind.Substraction:
-                        return (int)left - (int)right;
-                    case BoundBinaryOperatorKind.Multiplication:
-                        return (int)left * (int)right;
-                    case BoundBinaryOperatorKind.Division:
-                        return (int)left / (int)right;
-                    case BoundBinaryOperatorKind.LogicalAnd:
-                        return (bool)left && (bool)right;
-                    case BoundBinaryOperatorKind.LogicalOr:
-                        return (bool)left || (bool)right;
-                    case BoundBinaryOperatorKind.Equals:
-                        return Equals(left, right);
-                    case BoundBinaryOperatorKind.NotEquals:
-                        return !Equals(left, right);
-                    default:
-                        throw new Exception($"Unexpected operator: {b.Op.Kind}");
-                }
+                case BoundBinaryOperatorKind.Addition:
+                    return (int)left + (int)right;
+                case BoundBinaryOperatorKind.Substraction:
+                    return (int)left - (int)right;
+                case BoundBinaryOperatorKind.Multiplication:
+                    return (int)left * (int)right;
+                case BoundBinaryOperatorKind.Division:
+                    return (int)left / (int)right;
+                case BoundBinaryOperatorKind.LogicalAnd:
+                    return (bool)left && (bool)right;
+                case BoundBinaryOperatorKind.LogicalOr:
+                    return (bool)left || (bool)right;
+                case BoundBinaryOperatorKind.Equals:
+                    return Equals(left, right);
+                case BoundBinaryOperatorKind.NotEquals:
+                    return !Equals(left, right);
+                default:
+                    throw new Exception($"Unexpected operator: {b.Op.Kind}");
             }
-            throw new Exception($"Unexpected node: {node.Kind}");
         }
     }
 }
