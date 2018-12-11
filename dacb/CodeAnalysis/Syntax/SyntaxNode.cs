5fce8e56ba3b43d4d8942ae1f4ddc860e16f1b19
unknown <alberto.colom.monfort@gmail.com>
2018-12-11 09:56:32 +0100

    Mover pretty print a SyntaxNode

--- dacb/CodeAnalysis/Syntax/SyntaxNode.cs
+++ dacb/CodeAnalysis/Syntax/SyntaxNode.cs
@@ -1,4 +1,6 @@
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Reflection;
 
 namespace Dacb.CodeAnalysis.Syntax
@@ -6,6 +8,16 @@ namespace Dacb.CodeAnalysis.Syntax
     public abstract class SyntaxNode 
     {
         public abstract SyntaxKind Kind { get; }
+
+        public virtual TextSpan Span
+        {
+            get 
+            {
+                var first = GetChildren().First().Span;
+                var last = GetChildren().Last().Span;
+                return TextSpan.FromBounds(first.Start, last.End);
+            }
+        }
         public IEnumerable<SyntaxNode> GetChildren()
         {
             var properties = GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance);
@@ -22,6 +34,44 @@ namespace Dacb.CodeAnalysis.Syntax
                     foreach(var child in children)
                         yield return child;
                 }
+            }
+        }
+
+        public void WriteTo(TextWriter textWriter)
+        {
+            PrettyPrint(textWriter, this);
+        }
+
+        private static void PrettyPrint(TextWriter textWriter, SyntaxNode node, string indent = "", bool isLast = true)
+        {
+            // ├──
+            // │
+            // └──
+
+            var marker = isLast ? "└──" : "├──";
+            textWriter.Write(indent);
+            textWriter.Write(marker);
+            textWriter.Write(node.Kind);
+            if (node is SyntaxToken t && t.Value != null)
+            {
+                textWriter.Write(" ");
+                textWriter.Write(t.Value);
+            }
+            textWriter.WriteLine();
+            
+            indent += isLast ? "   " : "│  ";
+            
+            var lastChild = node.GetChildren().LastOrDefault();
+            foreach(var child in node.GetChildren())
+                PrettyPrint(textWriter, child, indent, lastChild == child);
+        }
+
+        public override string ToString() 
+        {
+            using(var writer =  new StringWriter())
+            {
+                this.WriteTo(writer);
+                return writer.ToString();
             }
         }
     }
--- dacb/CodeAnalysis/Syntax/SyntaxToken.cs
+++ dacb/CodeAnalysis/Syntax/SyntaxToken.cs
@@ -19,6 +19,6 @@ namespace Dacb.CodeAnalysis.Syntax
         public string Text { get; }
         public object Value { get; }
 
-        public TextSpan Span => new TextSpan(Position, Text.Length);
+        public override TextSpan Span => new TextSpan(Position, Text.Length);
     }
 }
