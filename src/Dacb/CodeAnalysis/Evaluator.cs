dcad59a46098b3293faa0c76531b31b67ecd48aa
unknown <alberto.colom.monfort@gmail.com>
2018-12-19 13:22:04 +0100

    Añadido soporte para operacones binaria BitwiseAnd, BitwiseOr, BitwiseXor y ~ que no sabia ni que existia

--- src/Dacb/CodeAnalysis/Evaluator.cs
+++ src/Dacb/CodeAnalysis/Evaluator.cs
@@ -141,6 +141,8 @@ namespace Dacb.CodeAnalysis
                     return -(int)operand;
                 case BoundUnaryOperatorKind.LogicalNegation:
                     return !(bool)operand;
+                case BoundUnaryOperatorKind.OnesComplement:
+                    return ~(int)operand;
 
                 default:
                     throw new Exception($"Unexpected unary operator '{u.Op}'");
@@ -160,6 +162,21 @@ namespace Dacb.CodeAnalysis
                     return (int)left * (int)right;
                 case BoundBinaryOperatorKind.Division:
                     return (int)left / (int)right;
+                case BoundBinaryOperatorKind.BitwiseAnd:
+                    if (b.Type == typeof(int))
+                        return (int)left & (int)right;
+                    else
+                        return (bool)left & (bool)right;
+                case BoundBinaryOperatorKind.BitwiseOr:
+                    if (b.Type == typeof(int))
+                        return (int)left | (int)right;
+                    else
+                        return (bool)left | (bool)right;
+                case BoundBinaryOperatorKind.BitwiseXor:
+                    if (b.Type == typeof(int))
+                        return (int)left ^ (int)right;
+                    else
+                        return (bool)left ^ (bool)right;
                 case BoundBinaryOperatorKind.LogicalAnd:
                     return (bool)left && (bool)right;
                 case BoundBinaryOperatorKind.LogicalOr:
